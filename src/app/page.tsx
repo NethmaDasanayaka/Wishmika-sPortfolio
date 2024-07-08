d8c40c6deffddd4cec2f9bb91cf736b9c6d748ca
NCDasanayaka <108860996+NCDasanayaka@users.noreply.github.com>
2024-07-08 13:04:47 +0530

    feat: fix issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
     <Container>
       <span className="text-4xl">👋</span>
       <Heading className="font-black">
-        Hello there! I&apos;m Wishmika Dasanayaka
+        {`Hello there! I'm Wishmika Dasanayaka`}
       </Heading>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a <Highlight>Data Scientist</Highlight> passionate about leveraging <Highlight>data</Highlight>{" "}
+        {`I'm a <Highlight>Data Scientist</Highlight> passionate about leveraging <Highlight>data</Highlight>{" "}
         to build <Highlight>products</Highlight> and <Highlight>web applications</Highlight> that can impact
-        millions of lives.
+        millions of lives.`}
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
       With <Highlight>one year of experience</Highlight>, I have developed expertise in creating <Highlight>data-driven solutions</Highlight> that are both effective and insightful. My focus is on ensuring that these solutions are <Highlight>scalable, performance-optimized</Highlight>, and <Highlight>visually compelling</Highlight>, consistently meeting both <Highlight>user</Highlight> and <Highlight>business needs</Highlight>.
@@ -27,7 +27,7 @@ export default function Home() {
         as="h2"
         className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
       >
-        What I&apos;ve been working on
+        {`What I've been working on`}
       </Heading>
       <Products />
       <TechStack />
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -38,70 +38,68 @@ export default function About() {
             />
           </motion.div>
         ))}
-        {/* 
-        // <Image
-        //   src="https://images.unsplash.com/photo-1692544350322-ac70cfd63614?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
-        //   width={200}
-        //   height={400}
-        //   alt="about"
-        //   className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
-        // />
-        // <Image
-        //   src="https://images.unsplash.com/photo-1692374227159-2d3592f274c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
-        //   width={200}
-        //   height={400}
-        //   alt="about"
-        //   className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
-        // />
-        // <Image
-        //   src="https://images.unsplash.com/photo-1692005561659-cdba32d1e4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        //   width={200}
-        //   height={400}
-        //   alt="about"
-        //   className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
-        // />
-        // <Image
-        //   src="https://images.unsplash.com/photo-1692445381633-7999ebc03730?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        //   width={200}
-        //   height={400}
-        //   alt="about"
-        //   className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
-        // /> */}
       </div>
 
-      <div className="max-w-4xl">
+      {/* <div className="max-w-4xl">
         <Paragraph className=" mt-4">
-          Hey there, I&apos;m Wishmika Dasanayaka - a passionate Data Scientist,
+          {`Hey there, I'm Wishmika Dasanayaka - a passionate Data Scientist,
           avid writer, and a connoisseur of awesome design. Welcome to my corner
-          of the digital world!
+          of the digital world!`}
         </Paragraph>
         <Paragraph className=" mt-4">
-        As a dedicated and driven fourth-year student at the University of Colombo, pursuing an Honors degree in IT and Management, my academic journey has been shaped by a steadfast commitment to excellence and a fervent passion for the fields of Data Science and AI.
+          As a dedicated and driven fourth-year student at the University of
+          Colombo, pursuing an Honors degree in IT and Management, my academic
+          journey has been shaped by a steadfast commitment to excellence and a
+          fervent passion for the fields of Data Science and AI.
         </Paragraph>
 
         <Paragraph className=" mt-4">
-        Having completed my secondary education at Bandaranayaka College Gampaha, I embarked on a path of exploration and growth, fueled by an insatiable curiosity for cutting-edge technologies. Throughout my university years, I have immersed myself in the intricate realm of Data Science and AI, delving into the intricacies of algorithms, predictive modeling, and machine learning frameworks.
+          Having completed my secondary education at Bandaranayaka College
+          Gampaha, I embarked on a path of exploration and growth, fueled by an
+          insatiable curiosity for cutting-edge technologies. Throughout my
+          university years, I have immersed myself in the intricate realm of
+          Data Science and AI, delving into the intricacies of algorithms,
+          predictive modeling, and machine learning frameworks.
         </Paragraph>
         <Paragraph className=" mt-4">
-        My enthusiasm for these transformative disciplines is evident through my proactive engagement in coursework, collaborative projects, and independent research. I have actively sought opportunities to expand my practical skills, honing my ability to extract insights from complex datasets, design meaningful experiments, and employ statistical methods to drive informed decision-making.
+          My enthusiasm for these transformative disciplines is evident through
+          my proactive engagement in coursework, collaborative projects, and
+          independent research. I have actively sought opportunities to expand
+          my practical skills, honing my ability to extract insights from
+          complex datasets, design meaningful experiments, and employ
+          statistical methods to drive informed decision-making.
         </Paragraph>
         <Paragraph className=" mt-4">
-        Beyond the classroom, my zeal for Data Science and AI has led me to actively participate in workshops, seminars, and hackathons, where I've had the privilege to collaborate with fellow enthusiasts and industry experts. These experiences have not only enriched my technical proficiency but have also nurtured my aptitude for teamwork, problem-solving, and effective communication.
+          Beyond the classroom, my zeal for Data Science and AI has led me to
+          actively participate in workshops, seminars, and hackathons, where
+          I've had the privilege to collaborate with fellow enthusiasts and
+          industry experts. These experiences have not only enriched my
+          technical proficiency but have also nurtured my aptitude for teamwork,
+          problem-solving, and effective communication.
+        </Paragraph>
+        <Paragraph className=" mt-4">
+          As I approach the culmination of my academic journey, I am resolute in
+          my aspiration to channel my knowledge and passion into a professional
+          career in the realm of Data Science and AI. Eager to contribute my
+          skills to real-world challenges, I am wholeheartedly committed to
+          leveraging data-driven insights and innovative solutions to shape the
+          future of industries and drive transformative growth.
+        </Paragraph>
 
-        </Paragraph>
-        <Paragraph className=" mt-4">
-        As I approach the culmination of my academic journey, I am resolute in my aspiration to channel my knowledge and passion into a professional career in the realm of Data Science and AI. Eager to contribute my skills to real-world challenges, I am wholeheartedly committed to leveraging data-driven insights and innovative solutions to shape the future of industries and drive transformative growth.
-        </Paragraph>
-        
-        <Paragraph className=" mt-4">
-        With a solid foundation in IT and Management, coupled with a fervent dedication to Data Science and AI, I am poised to embark on a journey that embodies both intellectual rigor and unwavering enthusiasm. I am excited about the prospect of applying my unique blend of skills, knowledge, and passion to make a lasting impact in the dynamic landscape of Data Science and AI.
+        <Paragraph className="mt-4">
+          With a solid foundation in IT and Management, coupled with a fervent
+          dedication to Data Science and AI, I am poised to embark on a journey
+          that embodies both intellectual rigor and unwavering enthusiasm. I am
+          excited about the prospect of applying my unique blend of skills,
+          knowledge, and passion to make a lasting impact in the dynamic
+          landscape of Data Science and AI.
         </Paragraph>
 
-        <Paragraph className=" mt-4">
-          Thank you for being here, and I can&apos;t wait to embark on this
-          adventure with you.
+        <Paragraph className="mt-4">
+          {`Thank you for being here, and I can't wait to embark on this
+          adventure with you.`}
         </Paragraph>
-      </div>
+      </div> */}
     </div>
   );
 }
